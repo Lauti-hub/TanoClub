8f2c0cab0e435fc31692a637dcfc1f0c45c77061
Lauti-hub <lauti2da491@gmail.com>
2025-09-02 23:43:59 -0300

    b.01

---
+++ TanoClub/config/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# SEGURIDAD
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# BASE DE DATOS
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = 'static/'
+
